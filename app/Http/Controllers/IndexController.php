44a8dbbbb74c5c46b26bb2140ca1b9b247db07e7
zhaoliangshan <995529442@qq.com>
2018-08-03 13:59:53 +0800

    测试

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -16,9 +16,6 @@ class IndexController extends Controller
      */
     public function index()
     {
-        $a = DB::table("admins")->first();
-        var_dump($a);
-        exit;
         $admins = Auth::guard("admins")->user();
 
         $username = $admins->username;  //用户名
