84f07dfa5bb1c772f1b23391eb2416d1a46a337d
zhaoliangshan <995529442@qq.com>
2018-08-04 11:43:35 +0800

    84

--- app/Http/Controllers/cater/CaterShopController.php
+++ app/Http/Controllers/cater/CaterShopController.php
@@ -14,6 +14,9 @@ class CaterShopController extends Controller
 {
     //微餐饮-后台首页
     public function index(){
+      var_dump(time());
+      var_dump(date("Y-m-d H:i:s",time()));
+      exit;
         $admins   = Auth::guard('admins')->user();
         $admin_id = $admins->id; 
         //获取省份数据
