a87d3f5394a5f7eb628d6b862d8b5fb1461a597f
sam <995529442@.com>
2018-08-07 19:33:01 +0800

    8.7 19:32

--- database/migrations/2018_05_19_132121_create_cater_orders_table.php
+++ database/migrations/2018_05_19_132121_create_cater_orders_table.php
@@ -23,6 +23,7 @@ class CreateCaterOrdersTable extends Migration
             $table->string('batchcode',20)->default('')->index()->comment("订单号");
             $table->tinyInteger('pay_type')->defalut('0')->comment("是否支付 0未支付 1已支付");
             $table->tinyInteger('status')->defalut('0')->comment("状态 -1已取消 0待付款 1待接单(已支付) 2已接单 3配送中 4配送完成 5已完成 6申请退款 7已退款 8拒绝退款 9已拒单");
+            $table->integer('create_time')->default('0')->comment("下单时间");
             $table->integer('pay_time')->default('0')->comment("支付时间");
             $table->integer('shipping_time')->default('0')->comment("配送时间");
             $table->integer('shipping_con_time')->default('0')->comment("配送完成时间");
