95706a15d84ce5d35f063c5a80ff8e1b37cb314e
zhaoliangshan <995529442@qq.com>
2018-08-15 17:05:21 +0800

    815 13:34

--- app/Http/Controllers/cater/Api/orderController.php
+++ app/Http/Controllers/cater/Api/orderController.php
@@ -257,11 +257,11 @@ class orderController extends Controller
                     array_push($order_goods_arr, $goods_data);
 
                     $goods_name_str .= $goods_info->good_name.$v['number']."份".$money."元\\n";
-                    $goods_name_mail_str .= $goods_info->good_name.$v['number']."份  ".$money."元<br /><span style='visibility:hidden;'>商品名称：</span>";
+                    $goods_name_mail_str .= "商品名称：</span>".$goods_info->good_name.$v['number']."份  ".$money."元<br /><span style='visibility:hidden;'>";
                 }
 
                 $goods_name_str = trim($goods_name_str,"\\n");
-                $goods_name_mail_str = trim($goods_name_mail_str,"<br /><span style='visibility:hidden;'>商品名称：</span>");
+                $goods_name_mail_str = trim($goods_name_mail_str,"<br /><span style='visibility:hidden;'>");
              }
               //真实支付金额，菜品总额+配送+包装
              $real_pay = $total_money + $shipping_fee + $package_fee;
@@ -294,7 +294,7 @@ class orderController extends Controller
                 $content = "您有一笔新的订单，请及时处理！！！<br />";
                 $content .= "订单编号：".$batchcode."<br />";
                 $content .= "订单类型：".$cater_type_name."<br />";
-                $content .= "商品名称：".$goods_name_mail_str."<br />";
+                $content .= $goods_name_mail_str."<br />";
                 $content .= "支付方式：微信支付<br />";
                 $content .= "支付金额：".$real_pay."<br />";
                 $content .= "支付时间：".date("Y-m-d H:i:s",time())."<br />";
