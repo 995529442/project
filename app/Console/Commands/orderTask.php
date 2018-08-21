a426f5d6c2c21dc7e30530cc5e821ef90e522d0f
zhaoliangshan <995529442@qq.com>
2018-08-21 11:48:38 +0800

    821 11:48

--- app/Console/Commands/orderTask.php
+++ app/Console/Commands/orderTask.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+date_default_timezone_set('PRC');
 use Illuminate\Console\Command;
 use DB;
 
@@ -39,27 +40,50 @@ class orderTask extends Command
     public function handle()
     {
         $this->cancel_order();   //未支付订单过期取消订单
+        $this->done_order();     //配送完成订单三日未处理自动已完成
     }
 
     /**
      * 未支付订单过期取消订单
      */
     public function cancel_order(){
-        $order_list = DB::table("cater_orders")->where(['pay_type'=>0,'status'=>0,'isvalid'=>true])->select(['id as order_id','create_time'])->get();
+        DB::table("cater_orders")->where(['pay_type'=>0,'status'=>0,'isvalid'=>true])->select(['id as order_id','create_time'])->orderBy("id","desc")->chunk(100,function($order_list){
+            if(!empty($order_list)){
+                foreach($order_list as $v){
+                    $create_time = $v->create_time;
 
-        if($order_list){
-            foreach($order_list as $v){
-                $create_time = $v->create_time;
+                    if($create_time){
+                        $now_time = date("Y-m-d",time());
+                        $days = (strtotime($now_time)-strtotime(date("Y-m-d",$create_time)))/86400;
 
-                if($create_time){
-                    $now_time = date("Y-m-d",time());
-                    $days = (strtotime($now_time)-strtotime(date("Y-m-d",$create_time)))/86400;
-
-                    if($days > 0){ //已过期
-                       DB::table("cater_orders")->whereId($v->order_id)->update(['status'=>-1]);
+                        if($days > 0 ){ 
+                           DB::table("cater_orders")->whereId($v->order_id)->update(['status'=>-1]);
+                        }
                     }
-                }
+                }  
             }
-        }
+         });
+    }
+
+    /**
+     * 未支付订单过期取消订单
+     */
+    public function done_order(){
+         DB::table("cater_orders")->where(['pay_type'=>1,'status'=>4,'isvalid'=>true])->select(['id as order_id','create_time'])->orderBy("id","desc")->chunk(100,function($order_list){
+            if(!empty($order_list)){
+                foreach($order_list as $v){
+                    $create_time = $v->create_time;
+
+                    if($create_time){
+                        $now_time = date("Y-m-d",time());
+                        $days = (strtotime($now_time)-strtotime(date("Y-m-d",$create_time)))/86400;
+
+                        if($days >=3 ){ 
+                           DB::table("cater_orders")->whereId($v->order_id)->update(['status'=>5]);
+                        }
+                    }
+                }  
+            }
+         });
     }
 }
