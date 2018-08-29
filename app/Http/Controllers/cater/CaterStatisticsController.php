0994d9b41c5240670d4457de172ad6cf54f0e95c
zhaoliangshan <995529442@qq.com>
2018-08-29 15:36:26 +0800

    829 15:14

--- app/Http/Controllers/cater/CaterStatisticsController.php
+++ app/Http/Controllers/cater/CaterStatisticsController.php
@@ -20,7 +20,7 @@ class CaterStatisticsController extends Controller
 	//微餐饮-统计首页
 	public function index(Request $request)
 	{
-		var_dump("Y-m-d H:i:s",time());
+		var_dump(date("Y-m-d H:i:s",time()));
 		$admins = Auth::guard('admins')->user();
 		$admin_id = $admins->id;
 		//当前日期
