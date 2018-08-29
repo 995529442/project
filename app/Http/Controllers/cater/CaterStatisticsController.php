7827a5cb95243a6e7708595feef220874649a0ef
zhaoliangshan <995529442@qq.com>
2018-08-29 15:38:16 +0800

    829 15:14

--- app/Http/Controllers/cater/CaterStatisticsController.php
+++ app/Http/Controllers/cater/CaterStatisticsController.php
@@ -8,6 +8,8 @@
 
 namespace App\Http\Controllers\cater;
 
+date_default_timezone_set('PRC');
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
